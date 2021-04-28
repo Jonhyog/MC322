eaf2fb790de2ef1a13d37202346b31b506ec3000
Jonhyog <jm.og@hotmail.com>
2021-04-28 11:42:06 -0300

    Fix deltaY and deltaX calculation in isValid

--- lab05/src/mc322/lab05/Dama.java
+++ lab05/src/mc322/lab05/Dama.java
@@ -1,10 +1,13 @@
 package mc322.lab05;
 
 public class Dama {
+	private int x, y;
     boolean branco;
 
-    Dama(boolean pBranco){
+    Dama(boolean pBranco, int x, int y){
         branco = pBranco;
+        this.x = x;
+        this.y = y;
     }
     
     public boolean getColor() {
@@ -17,13 +20,22 @@ public class Dama {
     	else
     		return 'P';
     }
+    
+    public void updatePosition(int x, int y) {
+    	this.x = x;
+    	this.y = y;
+    }
+    
+    public int[] getPosition() {
+    	return new int[] {x, y};
+    }
 
     boolean isValid(int []source, int []target, boolean comer){
         boolean valid = false;
         boolean compatible;
         int y, x;
-        y = target[0] - source[0];
-        x = Math.abs(target[1] - source[1]);
+        y = target[1] - source[1];
+        x = Math.abs(target[0] - source[0]);
         compatible = isCompatible(x, y);
 
         if(compatible == false){
